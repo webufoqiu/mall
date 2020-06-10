b3e7f15336e5fedb83feb929231072deb50d2bb7
webufoqiu <webufo@126.com>
2020-06-10 23:00:17 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,21 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+// import axios from 'axios'
+import jsonp from 'jsonp'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted(){
+    // let url1 = "https://easy-mock.com/mock/5ee0dade81099f496759c527/example/mock"
+    // axios.get(url1).then(()=>{ })
+    let url2='https://www.imooc.com/activity/servicetime';
+    jsonp(url2,()=>{ })
   }
+  // 跨域测试
 }
 </script>
 
